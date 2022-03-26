bd098382fe72a4d3ddd8d011e49ae09211cc8cea
rush-kh <sizonenko.oleg.kh@gmail.com>
2022-03-26 23:02:10 +0200

    Test2

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,62 @@
-let a = 1;
+// let variable1 = 88;
+// console.log(typeof variable1);
+
+ // const userAgeStr = prompt('How old are you?');
+ // const userAgeNum = Number(userAgeStr)
+
+ // console.log(typeof userAgeStr);
+ // console.log(userAgeStr);
+ // console.log(typeof userAgeNum);
+ // console.log(userAgeNum);
+
+
+// let font = '2.57988rem';
+// const integer = parseInt(font);
+// console.log(typeof integer);
+// console.log(integer);
+// const float = parseFloat(font);
+// console.log(typeof float);
+// console.log(float);
+
+
+// const message = "JavaScript is awesome!";
+// alert(message);
+
+// const isStudent = confirm('Are you a student?');
+// console.log(isStudent);
+
+// const appleCount = 7;
+// const grapeCount = 3;
+// const total = appleCount + grapeCount;
+// console.log('total', total);
+
+// let a = 5;
+// a = a + 1;
+// console.log(a);
+
+// a += 1;
+// console.log(a);
+// const a = 15;
+// const b = 5;
+// console.log(a % b);
+// console.log('2' > '13');
+
+// const isComing = confirm("Please confirm hotel reservation");
+// console.log(isComing);
+
+// const hotelName = prompt("Please enter desired hotel name");
+// console.log(hotelName);
+
+// const x = 5;
+// const y = 10;
+// const z = 5;
+// console.log("x > y:", x > y); // false
+
+// console.log(0.1 + 0.7);
+
+// console.log(0.17 + 0.24); // 0.41000000000000003
+// console.log((0.17 * 10 + 0.24 * 10) / 10); // 0.41
+
+// console.log(0.17 + 0.24); // 0.41000000000000003
+// console.log((0.17 + 0.24).toFixed(2)); // 0.41
+
